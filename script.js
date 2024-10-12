f39eede43e33bdc71cfbd50cea94211a67921519
ANGLT12345 <115406482+ANGLT12345@users.noreply.github.com>
2024-10-12 17:48:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    
-    if (sidebar.style.left === '0px') {
-        sidebar.style.left = '-250px'; // Hide the sidebar
-    } else {
-        sidebar.style.left = '0px'; // Show the sidebar
-    }
-}
+
